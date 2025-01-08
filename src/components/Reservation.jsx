290df95ae186bfc553ee752d91321a4fbcf82a94
OtarKapatadze <o.kapatadze@gmail.com>
2025-01-08 10:55:53 -0500

    made and styles reservation from

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect, useRef } from "react";
+
 export default function Reservation({ children, title = "Reservation page" }) {
   return (
-    <section className="router-section router-reservation">
-      <h2>{title}</h2>
-      {children}
-    </section>
+    <>
+      <section className="router-section router-reservation">
+        <form>
+          <label htmlFor="res-date">Choose date</label>
+          <input type="date" id="res-date" />
+          <label htmlFor="res-time">Choose time</label>
+          <input type="time" id="res-time" min="09:00" max="18:00" required />
+          <label htmlFor="guests">Number of guests</label>
+          <input type="number" placeholder="1" min="1" max="10" id="guests" />
+          <label htmlFor="occasion">Occasion</label>
+          <select id="occasion">
+            <option>Birthday</option>
+            <option>Anniversary</option>
+            <option>Friends</option>
+          </select>
+          <input className="button" type="submit" value="SUBMIT" />
+        </form>
+        {children}
+      </section>
+    </>
   );
 }
