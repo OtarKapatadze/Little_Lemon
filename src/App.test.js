a28a1c99c3b2a746f1b6b5042098426f8aafef67
OtarKapatadze <o.kapatadze@gmail.com>
2025-01-11 20:17:17 -0500

    nothing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render,fireEvent, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders the BookingForm heading', () => {
+  render(<BookingForm />);
+  const HeadingElement = screen.getByText();
   expect(linkElement).toBeInTheDocument();
 });
- 
+
+
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
