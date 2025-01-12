15b55d9c0ac34089f6660a997bc349d3ce3dcffe
OtarKapatadze <o.kapatadze@gmail.com>
2025-01-12 18:52:08 -0500

    capstone for coursera done i guess

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -2,8 +2,8 @@ import AvailableTimes from "./AvailableTimes";
 import { useEffect, useState } from "react";
 import { fetchAPI, submitAPI } from "../api";
 import { useNavigate } from "react-router";
-// import { object, string, number, date, InferType } from "yup";
-// import { Formik } from "formik";
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup"; // Add Yup for validation
 
 export default function BookingForm({
   formData,
@@ -12,10 +12,10 @@ export default function BookingForm({
   availableTimes,
 }) {
   const [resetTimes, setResetTimes] = useState(availableTimes);
-  const [isSubmited, setIsSubmitted] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const navigate = useNavigate();
 
-  // useEffect for resetTImes
+  // useEffect for resetTimes
   useEffect(() => {
     const today = new Date();
     const todaysAvailability = fetchAPI(today);
@@ -23,9 +23,8 @@ export default function BookingForm({
   }, []);
 
   // useEffect for submission
-
   useEffect(() => {
-    if (isSubmited) {
+    if (isSubmitted) {
       const submited = submitAPI(formData);
       if (submited) {
         navigate("/ConfirmedBooking");
@@ -33,34 +32,29 @@ export default function BookingForm({
     }
 
     setIsSubmitted(false);
-    // console.log(isSubmited);
-  }, [isSubmited]);
+  }, [isSubmitted]);
 
-  // handleChange updated formData changes
-  function handleChange(e) {
-    const { id, value } = e.target;
-    if (id === "date") {
-      dispatch({ type: "initialize_times", payload: resetTimes });
-      setFormData((prev) => ({ ...prev, [id]: value }));
-    } else {
-      setFormData((prevData) => {
-        return {
-          ...prevData,
-          [id]: value,
-        };
-      });
-    }
-  }
+  // Validation schema using Yup
 
-  // Submit Handler
+  const validationSchema = Yup.object({
+    date: Yup.date().required("Date is required"),
+    time: Yup.string().required("Time is required"), // Add validation for time
+    guests: Yup.number()
+      .min(1, "Guests must be at least 1")
+      .max(10, "Guests cannot exceed 10")
+      .required("Guests are required"),
+    occasion: Yup.string().required("Please select an occasion"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+  });
 
-  // i need to make submit so that once submitted in reducer changes initial value
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formData);
-    dispatch({ type: "available_times", payload: formData.time });
+  // handleSubmit
+  const handleSubmit = (values) => {
+    // Submit form data and dispatch to change state
+    dispatch({ type: "available_times", payload: values.time });
+
     // Optionally reset the form data
-
     setFormData({
       date: "",
       time: "",
@@ -69,72 +63,118 @@ export default function BookingForm({
       email: "",
     });
 
-    setIsSubmitted(!isSubmited);
-  }
+    setIsSubmitted(true);
+  };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="date" className="label">
-          Choose date
-        </label>
-        <input
-          value={formData.date}
-          onChange={handleChange}
-          type="date"
-          id="date"
-        />
-        <AvailableTimes
-          time={formData.time}
-          handleChange={handleChange}
-          availableTimes={availableTimes}
-        />
-        <label htmlFor="guests" className="label">
-          Number of guests
-        </label>
-        <input
-          value={formData.guests}
-          onChange={handleChange}
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          id="guests"
-        />
-        <label htmlFor="occasion" className="label">
-          Occasion
-        </label>
-        <select
-          value={formData.occasion}
-          onChange={handleChange}
-          id="occasion"
-          className="select select-occasion"
-        >
-          <option className="option" value="What Occasion">
-            What Occasion ?
-          </option>
-          <option className="option" value={"Birthday"}>
-            Birthday
-          </option>
-          <option className="option" value={"Anniversary"}>
-            Anniversary
-          </option>
-          <option className="option" value={"Friends"}>
-            Friends
-          </option>
-        </select>
-        <label htmlFor="email" className="label">
-          Email
-        </label>
-        <input
-          value={formData.email}
-          onChange={handleChange}
-          type="email"
-          placeholder="....@mail.com"
-          id="email"
-        />
-        <input className="button" type="submit" value="SUBMIT" />
-      </form>
-    </>
+    <Formik
+      initialValues={formData}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ values, handleChange, handleBlur, errors, touched }) => (
+        <Form>
+          <label htmlFor="date" className="label">
+            Choose date
+          </label>
+          <Field
+            value={values.date}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="date"
+            id="date"
+            name="date"
+          />
+          {touched.date && errors.date && (
+            <div className="error">{errors.date}</div>
+          )}
+
+          <label htmlFor="time" className="label">
+            Choose time
+          </label>
+          <Field
+            as="select"
+            value={values.time}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="time"
+            id="time"
+            className="select"
+          >
+            <option value="">Select Time</option>
+            {availableTimes.map((time, index) => (
+              <option key={index} value={time}>
+                {time}
+              </option>
+            ))}
+          </Field>
+          {touched.time && errors.time && (
+            <div className="error">{errors.time}</div>
+          )}
+
+          <label htmlFor="guests" className="label">
+            Number of guests
+          </label>
+          <Field
+            value={values.guests}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="number"
+            placeholder="1"
+            min="1"
+            max="10"
+            id="guests"
+            name="guests"
+          />
+          {touched.guests && errors.guests && (
+            <div className="error">{errors.guests}</div>
+          )}
+
+          <label htmlFor="occasion" className="label">
+            Occasion
+          </label>
+          <Field
+            as="select"
+            name="occasion"
+            id="occasion"
+            className="select select-occasion"
+          >
+            <option className="option" value="What Occasion">
+              What Occasion ?
+            </option>
+            <option className="option" value="Birthday">
+              Birthday
+            </option>
+            <option className="option" value="Anniversary">
+              Anniversary
+            </option>
+            <option className="option" value="Friends">
+              Friends
+            </option>
+          </Field>
+          {touched.occasion && errors.occasion && (
+            <div className="error">{errors.occasion}</div>
+          )}
+
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <Field
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            type="email"
+            placeholder="....@mail.com"
+            id="email"
+            name="email"
+          />
+          {touched.email && errors.email && (
+            <div className="error">{errors.email}</div>
+          )}
+
+          <input className="button" type="submit" value="SUBMIT" />
+        </Form>
+      )}
+    </Formik>
   );
 }
--- src/components/ConfirmedBooking.jsx
+++ src/components/ConfirmedBooking.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 export default function ConfirmedBooking({
   children,
   title = "Congrats You Just Booked a Table",
 }) {
   return (
-    <section className="router-section router-about">
+    <section className="router-section router-Confirmation">
       <h2>{title}</h2>
+      <Link className="linkforhome" to="/">
+        Home
+      </Link>
       {children}
     </section>
   );
